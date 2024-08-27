ff6889085cbb1f2bb5af0391d8c18290b9999527
Yani Degrande <Yani.degrande@student.hogent.be>
2024-08-27 22:20:28 +0200

    sensors

--- controls.js
+++ controls.js
@@ -1,43 +1,43 @@
-class Controls {
-    constructor() {
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+class Controls{
+    constructor(){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
         this.#addKeyboardListeners();
     }
 
-    #addKeyboardListeners() {
-        document.onkeydown = (event) => {
-            switch (event.key) {
-                case 'ArrowUp':
-                    this.forward = true;
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
                     break;
-                case 'ArrowLeft':
-                    this.left = true;
+                case "ArrowRight":
+                    this.right=true;
                     break;
-                case 'ArrowRight':
-                    this.right = true;
+                case "ArrowUp":
+                    this.forward=true;
                     break;
-                case 'ArrowDown':
-                    this.reverse = true;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
             }
         }
-        document.onkeyup = (event) => {
-            switch (event.key) {
-                case 'ArrowUp':
-                    this.forward = false;
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
                     break;
-                case 'ArrowLeft':
-                    this.left = false;
+                case "ArrowRight":
+                    this.right=false;
                     break;
-                case 'ArrowRight':
-                    this.right = false;
+                case "ArrowUp":
+                    this.forward=false;
                     break;
-                case 'ArrowDown':
-                    this.reverse = false;
+                case "ArrowDown":
+                    this.reverse=false;
                     break;
             }
         }
